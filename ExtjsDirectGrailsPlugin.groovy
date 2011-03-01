634a2ed6c34fc68d298869dfda06e92849eadfee
Matthias Hryniszak <padcom@gmail.com>
2011-03-02 00:28:20 +0100

    Updated basic plugin information

--- ExtjsDirectGrailsPlugin.groovy
+++ ExtjsDirectGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class ExtjsDirectGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0.0M1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.3.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -11,11 +11,12 @@ class ExtjsDirectGrailsPlugin {
     ]
 
     // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
+    def author = "Matthias Hryniszak"
+    def authorEmail = "padcom@gmail.com"
+    def title = "Ext.Direct support for Grails"
     def description = '''\\
-Brief description of the plugin.
+This plugin provides support for Ext.Direct from Ext JS library for Grails so that calling services 
+from JavaScript is possible and (above all) easy.
 '''
 
     // URL to the plugin's documentation
